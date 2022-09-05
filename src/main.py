f9920a62bb60abf306e8da82d36ac3cec0542248
Davi Marinho <davi-marinho@Davi-Marinho-PC.localdomain>
2022-09-04 22:42:05 -0300

    Add the genetic algorithm
    Signed-off-by: Davi Marinho <davi-marinho@Davi-Marinho-PC.localdomain>

--- src/main.py
+++ src/main.py
@@ -23,3 +23,48 @@ def initiate_neural_nets(x_train, y_train, initial_population: int = 20) -> List
                            output_weights=None, x_train=x_train, y_train=y_train))
 
     return neural_nets
+
+def genetic_algorithm(neural_nets: List, x, y) -> GenReturn:
+    fitness = 0
+    best_weights = []
+    output_layer_weights = []
+    computed_nns = []
+    gen = 0
+    log_file = open('log_file.txt', 'w')
+
+    while fitness < .85:
+        print(f"Generation {gen}")
+        gen += 1
+        for idx, neural_net in enumerate(neural_nets):
+            neural_net_fp_value = neural_net.forward_prop()
+            print(f"Neural Net {idx} score -> {neural_net_fp_value}")
+            computed_nns.append(neural_net)
+            log_file.write(str(neural_net.fitness_value) + '\n')
+
+        neural_nets.clear()
+
+        computed_nns = sorted(
+            computed_nns, key=lambda x: x.fitness_value, reverse=True)
+
+        for i in range(0, len(computed_nns)):
+            if computed_nns[i].fitness_value > fitness:
+                fitness = computed_nns[i].fitness_value
+                print(f"Gen {gen}")
+                print(f"Max fitness value -> {fitness}")
+                best_weights = []
+                output_layer_weights = []
+                for idx, layer in enumerate(computed_nns[i].layers):
+                    if idx != len(computed_nns) - 1:
+                        best_weights.append(layer.get_weights()[0])
+                    else:
+                        output_layer_weights.append(layer.get_weights()[0])
+
+        for i in range(0, 3):
+            for _ in range(0, 2):
+                tmp = crossover(computed_nns[i], 
+                                choice(computed_nns), 
+                                x_train=x, 
+                                y_train=y)
+                neural_nets.append(tmp)
+    log_file.close()
+    return (best_weights, output_layer_weights)
